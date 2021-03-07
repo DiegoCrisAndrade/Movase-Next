54e2364c6a8c7c2abff49d07a781ae9ffe280e84
Diego <diego_andrade_10@hotmail.com>
2021-03-07 16:39:58 -0300

    :package: Dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/DiegoCrisAndrade.png" alt="Diego Andrade" />
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Diego Andrade</strong>
                 <p>
                     <img src="icons/Level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
